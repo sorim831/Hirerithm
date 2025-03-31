b79df69ea5d0fbce794b45ab918521386a83c192
SongSeungGyu <sorim831@naver.com>
2025-03-31 19:28:16 +0900

    🔨 #5 DB 연결

--- Backend/server.js
+++ Backend/server.js
@@ -31,5 +31,4 @@ db.connect();
 const port = process.env.PORT || 5000; // 포트 설정
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
-  console.log(process.env.MONGODB_URI);
 });
