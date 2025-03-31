f5a1a1e858dadda10641e3398af3a3c96247bd0c
SongSeungGyu <sorim831@naver.com>
2025-03-31 19:54:10 +0900

    🎉 #5 Recruiter DB 모델 생성

---
+++ Backend/models/Recruiter.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const recruiterSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "올바른 이메일 형식이 아닙니다.",
+    ],
+  },
+  password_hash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    match: [/^\d{3}-?\d{4}-?\d{4}$/, "올바른 전화번호 형식이 아닙니다."],
+  },
+  role: {
+    type: String,
+    enum: ["headhunter", "company"],
+    required: true,
+  },
+  company_name: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Recruiter = mongoose.model("Recruiter", recruiterSchema);
+
+module.exports = Recruiter;
