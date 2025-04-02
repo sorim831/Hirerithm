073fa1e44d33caf4a3c37dfca7b86caf3f263739
GaYeon <yun02190219@gmail.com>
2025-04-03 01:49:22 +0900

    ✨ #8 기업이미지 기반 추천 페이지 레이아웃 코드 작성 - 전체 페이지에 적용되는 pages.css 코드작성 - 네비게이션 바 컴포넌트화 했음

--- Frontend/src/Pages/RecommendPages/CorporateImage.js
+++ Frontend/src/Pages/RecommendPages/CorporateImage.js
@@ -1,33 +1,22 @@
-import React, { useState, useEffect } from "react";
-import HirerithmLogo from "../../Image/logo/HirerithmLogo.svg";
+import React, { useState } from "react";
 import RecommendIcon from "../../Image/Icon/RecommendIcon.svg";
 import FileLogo from "../../Image/Icon/FileLogo.svg";
 import SearchIcon from "../../Image/Icon/SearchIcon.svg";
+import Navigation from "../../Component/Navigation";
+import "./corporateImage.css";
 
 const CorporateImage = () => {
+  const [companyName, setCompanyName] = useState("");
+
+  const handleSearch = () => {
+    console.log(`Searching for: ${companyName}`);
+    // 검색 로직 추가
+  };
+
   return (
     <div className="recommend-wrapper">
       {/* 네비게이션 */}
-      <nav>
-        <div className="navigation-wrapper">
-          {/* 하이어리즘 로고 */}
-          <div className="navigation-logo">
-            <img src={HirerithmLogo} alt="하이어리즘 (Hire + Algorithm)" />
-          </div>
-
-          <div className="navigation-buttons">
-            <ul>
-              <li>추천</li>
-              <li>DB열람</li>
-              <li>마이페이지</li>
-              <li>
-                {/* 로그아웃 버튼 */}
-                <button>로그아웃</button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navigation />
 
       <header>
         {/* 페이지 인덱스 */}
@@ -35,23 +24,28 @@ const CorporateImage = () => {
           <img src={FileLogo} alt="-" />
           <h2>기업 이미지 기반 추천</h2>
         </div>
-
         {/* 페이지 소개글 */}
         <p>
-          기업 이미지 기반 후보자 추천 페이지 입니다. 먼저, 기업명을
+          기업 이미지 기반 후보자 추천 페이지입니다. 먼저, 기업명을
           입력해주세요! 각 카테고리별 기업 이미지 점수를 열람할 수 있어요!
         </p>
       </header>
 
       <main>
         {/* 추천페이지 아이콘 */}
-        <img src={RecommendIcon} alt="기업이미지 기반 후보자 추천" />
+        <img src={RecommendIcon} alt="기업 이미지 기반 후보자 추천" />
+
         {/* 입력창 */}
         <div className="corporate-name-input-wrapper">
-          <input placeholder="이미지 키워드 점수를 낼 기업명을 입력하세요."></input>
-          <img src={SearchIcon} alt="기업 이름 검색하기" />
+          <input
+            value={companyName}
+            onChange={(e) => setCompanyName(e.target.value)}
+            placeholder="이미지 키워드 점수를 낼 기업명을 입력하세요."
+          />
+          <button onClick={handleSearch}>
+            <img src={SearchIcon} alt="기업 이름 검색하기" />
+          </button>
         </div>
-        {/* 추천 키워드 & 육각형 */}
       </main>
     </div>
   );
