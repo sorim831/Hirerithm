a49247ae21670db36d5e0058e74de8b1d271b144
SongSeungGyu <sorim831@naver.com>
2025-04-06 13:57:31 +0900

    ✨  #14 FE가 받기 쉽게 배열 형태로 응답

--- Backend/controllers/companyController.js
+++ Backend/controllers/companyController.js
@@ -14,15 +14,14 @@ exports.getCompanyKeywords = async (req, res) => {
         .json({ message: "해당 회사의 키워드를 찾을 수 없습니다." });
     }
 
-    const keywordData = {};
-    keywords.forEach((item) => {
-      keywordData[item.category] = {
-        score: item.score,
-        count: item.count,
-      };
-    });
+    // const keywordData = {};
+    const keywordArray = keywords.map((item) => ({
+      category: item.category,
+      score: item.score,
+      count: item.count,
+    }));
 
-    res.status(200).json({ keywordData });
+    res.status(200).json({ keywordArray });
   } catch (error) {
     console.error("기업 키워드 조회 오류:", error);
     res.status(500).json({ message: "서버 오류가 발생했습니다." });
