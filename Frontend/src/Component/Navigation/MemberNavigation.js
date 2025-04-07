289baead1885ee9f78a172c3ff32f9180dee4a7d
yun02 <yun02190219@gmail.com>
2025-04-07 19:28:39 +0900

    ⚡️ #30 네비게이션바 페이지 이동 기능 추가

--- Frontend/src/Component/Navigation/MemberNavigation.js
+++ Frontend/src/Component/Navigation/MemberNavigation.js
@@ -6,20 +6,27 @@ import { useNavigate } from "react-router-dom";
 const Navigation = () => {
   const navigate = useNavigate();
 
+  // 로그아웃 처리
   const handleLogout = () => {
-    // TODO: 로그아웃 로직 추가
+    localStorage.removeItem("token");
+    localStorage.removeItem("admin_info_id");
+
+    // 메인 페이지로 이동
+    navigate("/");
   };
 
   return (
     <nav className="navigation-wrapper">
       {/* 하이어리즘 로고 */}
-      <div className="navigation-logo">
+      <div className="navigation-logo" onClick={() => navigate("/")}>
         <img src={HirerithmLogo} alt="하이어리즘 (Hire + Algorithm)" />
       </div>
+
+      {/* 페이지이동 버튼들 */}
       <div className="navigation-buttons">
-        <button>추천</button>
-        <button>DB열람</button>
-        <button>마이페이지</button>
+        <button onClick={() => navigate("/recommend-strength")}>추천</button>
+        <button onClick={() => navigate("/full-view")}>DB열람</button>
+        <button onClick={() => navigate("/my-page")}>마이페이지</button>
         {/* 로그아웃 버튼 */}
         <button onClick={handleLogout}>로그아웃</button>
       </div>
