55b92cef402e79bc6f94ac91b4ea92e37d190dfe
SongSeungGyu <sorim831@naver.com>
2025-04-14 15:06:55 +0900

    🧹 #21 이력서 업로드 API 호출 수정

--- Backend/routes/resumeRoute.js
+++ Backend/routes/resumeRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const resumeController = require("../controllers/resumeController");
 
 // 이력서 업로드 엔드포인트
-router.get("/upload", resumeController.uploadResume);
+router.post("/upload", resumeController.uploadResume);
 
 module.exports = router;
