a0f66d855dffa870a2422f6f8333efa8458732c9
yun02 <yun02190219@gmail.com>
2025-04-26 19:47:10 +0900

    ✨ #57 기업 이미지 추천 결과 pdf 다운 기능 구현

--- Frontend/src/Component/RecommandComponent/CorporateImageCoverPage.js
+++ Frontend/src/Component/RecommandComponent/CorporateImageCoverPage.js
@@ -1,49 +1,83 @@
-import React from "react";
+import React, {
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import "./corporateImageCoverPage.css";
 
-const CorporateImageCoverPage = ({ summary }) => {
-  return (
-    <div className="image-recommend-result_report-content">
-      <div className="image-recommend-result_report-summary">
-        하이어리즘 /{" "}
-        <input
-          className="company-name-input"
-          placeholder="기업명 입력"
-          value={"oo 기업"}
-        />
-        추천 결과
-      </div>
-      <h3>기업 이미지 기반 [ oo 기업 마케팅 팀장 직무 ] 후보자 추천 결과</h3>
-      <input
-        className="image-recommend-result_report-author-input"
-        placeholder="보고서 작성자 정보를 입력하세요."
-        value={summary.author}
-      />
+const CorporateImageCoverPage = forwardRef(
+  ({ resultsSummary, companySummary, isGeneratingPdf }, ref) => {
+    const [companyName, setCompanyName] = useState("oo 기업");
+    const [authorInfo, setAuthorInfo] = useState("유니코서치 대리 김가연");
 
-      <div className="image-recommend-result_recommand-result-summary">
-        <h4>{summary.keywords}</h4>
-        <table className="report-candidate-table">
-          <thead>
-            <tr>
-              <th>순위</th>
-              <th>기본 인적사항</th>
-              <th>점수</th>
-            </tr>
-          </thead>
-          <tbody>
-            {summary.candidates.map((c) => (
-              <tr key={c.rank}>
-                <td>{c.rank}</td>
-                <td>{c.info}</td>
-                <td>{c.score}</td>
+    const localRef = useRef();
+
+    useImperativeHandle(ref, () => ({
+      getContent: () => localRef.current,
+    }));
+
+    return (
+      <div
+        ref={localRef}
+        className={`image-recommend-result_report-cover-page-content ${
+          isGeneratingPdf ? "pdf-mode" : ""
+        }`}
+      >
+        <div className="image-recommend-result_report-summary">
+          하이어리즘 / {companyName} 추천 결과
+        </div>
+        <h3>기업 이미지 기반 [ oo 기업 마케팅 팀장 직무 ] 후보자 추천 결과</h3>
+
+        <div className="image-recommend-result_report-author-input">
+          {authorInfo}
+        </div>
+
+        <div className="image-recommend-result_recommand-result-summary">
+          <h4>{resultsSummary.keywords}</h4>
+          <table className="image-recommend-result_report-candidate-table">
+            <thead>
+              <tr>
+                <th>순위</th>
+                <th>기본 인적사항</th>
+                <th>점수</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {resultsSummary.candidates.map((c) => (
+                <tr key={c.rank}>
+                  <td>{c.rank}</td>
+                  <td>{c.info}</td>
+                  <td>{c.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-      <p>자세한 후보자 이력서는 뒷장에 첨부.</p>
-    </div>
-  );
-};
+          <table className="image-recommend-result_report-candidate-table">
+            <thead>
+              <tr>
+                <th>순위</th>
+                <th>기업 이미지</th>
+                <th>점수</th>
+              </tr>
+            </thead>
+            <tbody>
+              {companySummary.map((c) => (
+                <tr key={c.rank}>
+                  <td>{c.rank}</td>
+                  <td>{c.subject}</td>
+                  <td>{c.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <p>자세한 후보자 이력서는 뒷장에 첨부.</p>
+      </div>
+    );
+  }
+);
 
 export default CorporateImageCoverPage;
--- Frontend/src/Component/RecommandComponent/StrengthCoverPage.js
+++ Frontend/src/Component/RecommandComponent/StrengthCoverPage.js
@@ -19,20 +19,24 @@ const StrengthCoverPage = forwardRef(({ summary, isGeneratingPdf }, ref) => {
   return (
     <div
       ref={localRef}
-      className={`report-cover-page-content ${
+      className={`strength-category-result_report-cover-page-content ${
         isGeneratingPdf ? "pdf-mode" : ""
       }`}
     >
-      <div className="report-summary">하이어리즘 / {companyName} 추천 결과</div>
+      <div className="strength-category-result_report-summary">
+        하이어리즘 / {companyName} 추천 결과
+      </div>
       <h3>
         강점 키워드 기반 [ {companyName} 마케팅 팀장 직무 ] 후보자 추천 결과
       </h3>
 
-      <div className="report-author-input">{authorInfo}</div>
+      <div className="strength-category-result_report-author-input">
+        {authorInfo}
+      </div>
 
-      <div className="recommand-result-summary">
+      <div className="strength-category-result_recommand-result-summary">
         <h4>[ 경력 ] [ 현장 경험 ] [ 매출 성장 기여 ] 강점 키워드</h4>
-        <table className="report-candidate-table">
+        <table className="strength-category-result_report-candidate-table">
           <thead>
             <tr>
               <th>순위</th>
