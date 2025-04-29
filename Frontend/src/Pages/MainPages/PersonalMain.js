4dd1a6e308c8b58f164ac2d09621f7daeb27a78e
yun02 <yun02190219@gmail.com>
2025-04-29 19:37:01 +0900

    💄 디자인 수정

--- Frontend/src/Pages/MainPages/PersonalMain.js
+++ Frontend/src/Pages/MainPages/PersonalMain.js
@@ -5,30 +5,10 @@ import { useNavigate } from "react-router-dom";
 import "./styles/PersonalMain.css";
 import NotMemberNavigation from "../../Component/Navigation/NotMemberNavigation";
 import ResumeRegistrationIcon from "../../Image/Icon/ResumeRegistrationIcon.svg";
-import CustomCorporateTestIcon from "../../Image/Icon/CustomCorporateTestIcon.svg";
-
-// 이미지 불러오기
-import img1 from "../../Image/Mainbanner/배너이미지1.png";
-import img2 from "../../Image/Mainbanner/배너이미지1.png";
-import img3 from "../../Image/Mainbanner/배너이미지1.png";
-
-const images = [img1, img2, img3];
+import BannerImage from "../../Image/Mainbanner/BannerImage.png";
 
 function PersonalMain() {
-  const [currentIndex, setCurrentIndex] = useState(0);
   const navigate = useNavigate();
-
-  // 10초마다 이미지 전환
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 10000);
-    return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 제거
-  }, []);
-
-  const handleDotClick = (index) => {
-    setCurrentIndex(index);
-  };
 
   return (
     <div className="personal-main_container">
@@ -36,18 +16,10 @@ function PersonalMain() {
       <NotMemberNavigation />
 
       {/* 배너 이미지 */}
-      <div className="banner">
-        <img src={images[currentIndex]} alt={`배너 ${currentIndex + 1}`} />
-        <div className="dots">
-          {images.map((_, i) => (
-            <span
-              key={i}
-              className={`dot ${i === currentIndex ? "active" : ""}`}
-              onClick={() => handleDotClick(i)}
-            ></span>
-          ))}
-        </div>
-      </div>
+      <div
+        className="banner"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
 
       {/* 이력서 등록 / 맞춤기업 테스트 이동 버튼 */}
       <div className="button-section">
@@ -61,15 +33,6 @@ function PersonalMain() {
           >
             <img src={ResumeRegistrationIcon} alt="📄" />
             <p>이력서 등록하러 가기</p>
-          </button>
-        </div>
-        <div className="button-wrapper">
-          <button
-            className="test-button"
-            onClick={() => navigate("/nonmember/companytest")}
-          >
-            <img src={CustomCorporateTestIcon} alt="🔎" />
-            <p>맞춤 기업 TEST</p>
           </button>
         </div>
       </div>
