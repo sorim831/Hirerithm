506e2691f2c85845df8c87cf9b56e4e302199857
SeungGyu <sorim831@naver.com>
2025-04-30 21:03:00 +0900

    ✨ #21 GPT API 연결

--- Backend/routes/resumeRoute.js
+++ Backend/routes/resumeRoute.js
@@ -5,10 +5,16 @@ const resumeController = require("../controllers/resumeController");
 // 이력서 업로드 엔드포인트
 router.post("/upload", resumeController.uploadResume);
 
+// 이력서 키워드 엔드포인트
+router.post("/:resume_id/keyword", resumeController.keywordResume);
+
+// 이력서 다운 엔드포인트  ( 안쓸듯? )
 router.get("/download/:filename", resumeController.downloadResume);
 
+// 이력서 리스트 엔드포인트
 router.get("/list", resumeController.listResume);
 
+// 이력서 상세 리스트 엔드포인트
 router.get("/list/:resume_id", resumeController.detaillistResume);
 
 module.exports = router;
