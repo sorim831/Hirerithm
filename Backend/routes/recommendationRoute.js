5175ddf61e05e0ff379f32af6b0238a63c58ace7
SeungGyu <sorim831@naver.com>
2025-05-15 23:06:06 +0900

    🐛 #92 모듈 로드 오류 해결

--- Backend/routes/recommendationRoute.js
+++ Backend/routes/recommendationRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const recommendationController = require("../controllers/recommendationyController");
-
+const recommendationController = require("../controllers/recommendationController");
 
 // 후보차 추천 엔드포인트
 router.post("/candidate", recommendationController.recommendCandidate);
